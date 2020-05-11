7feff722536627b439b780675455fdf3a8c477f7
whutfaker <1312976124@qq.com>
2020-05-11 20:05:24 +0800

    新增test

---
+++ test1/index.js
@@ -0,0 +1,6 @@
+alert("我是插件内容")
+// document.querySelectorAll('.eRxLTp').forEach(item=>{item.remove()})
+
+// GREKxL document.querySelectorAll('.GREKxL').forEach(item=>{item.remove()})
+// rRbDzk
+// 
